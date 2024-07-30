cbe3378b41767d6f059bac4fe333755862bd7b8c
Lucas Gabriel Babolim Vieira <125750275+LucasGabrielBVieira@users.noreply.github.com>
2024-07-30 16:42:26 -0300

    Declarações e Sintaxe

---
+++ Declarações e Sintaxe/js/script.js
@@ -0,0 +1,32 @@
+//As declaraçõe em java script normalmente são separadas em ;, porém, não é obrigatório
+
+let a, b, c;
+a = 9;
+b = 8;
+c = 7;
+
+//Essa mesma declaração pode ser feita da seguinte maneira
+
+let d, e, f; d = 6; e = 5; f = 4;
+
+//Também sem ponto e vírgula
+let g, h, i 
+g = 3
+h = 2
+i = 1
+
+//Quebras de linha também não provocão mudançãs nas declarações. Contanto que ; termine a declaração.
+
+// document.
+//     getElementById('Uva').
+//     innerHTML =
+//     "Funciona";
+
+//Esse hábito não é recomendável
+
+//A declaração de uma variável pode ser feita com var, let ou const
+var tres = 3;
+var soma = tres + 3;
+console.log(soma);
+
+//Para usar comentários, use //.
