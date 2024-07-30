ef0ef848f803faa2131d56b933b0f57ca70f02ed
Lucas Gabriel Babolim Vieira <125750275+LucasGabrielBVieira@users.noreply.github.com>
2024-07-30 16:07:03 -0300

    Tipos de saída

---
+++ Tipos de saída/js/script.js
@@ -0,0 +1,6 @@
+document.getElementById("texto").innerHTML = "É possível capturar uma tag HTML através do seu id, usando <strong>get.ElementById('id')</strong> e proporcionar uma saída em forma de documento HTML usando <strong>innerHTML</strong>.";
+
+alert("Alerta é uma forma de saída que pode receber funções ou textos. ex:")
+alert(10 + 10)
+
+console.log("Esse alerta é mais silencioso e é mostrado no console do navegador.")
